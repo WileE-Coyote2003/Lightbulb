e716a3bf8927a5fc9e286f95a8a160e29ed95f05
Thwin Htoo Aung <htoohtoo.mdy.mmr@gmail.com>
2026-06-23 08:49:06 +0700

    Initial commit

--- Lightbulb/ViewController.swift
+++ Lightbulb/ViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+
+    @IBOutlet weak var actionButton: UIButton!
+    
+    @IBOutlet weak var lightBulb: UIImageView!
+    
+    @IBOutlet weak var lightLabel: UILabel!
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func actionButtonPressed(_ sender: Any) {
+        lightOn.toggle()
+        changeLight()
+    }
+    fileprivate func changeLight() {
+        if lightOn{
+            lightBulb.tintColor = .yellow
+            lightLabel.text = "Light is ON"
+            lightLabel.textColor = .white
+            view.backgroundColor = .black
+            greetingLabel.text = "Hello \(textField.text!)"
+            greetingLabel.textColor = .yellow
+        }else{
+            lightBulb.tintColor = .black
+            lightLabel.text = "Light is OFF"
+            view.backgroundColor = .white
+            greetingLabel.text = "Hello \(textField.text!)"
+            greetingLabel.textColor = .yellow
+        }
     }
 
 
